be3bed2de729d3890cc5d8211141cd1acbd6bf26
jtwob <32282322+jtwob@users.noreply.github.com>
2022-07-19 14:05:26 -0700

    UPDATE: Appended books from api to catalogue state and rendered book titles on catalogue page

--- client/src/Pages/Catalogue.js
+++ client/src/Pages/Catalogue.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Catalogue = () => {
+  const [books, setBooks] = useState({
+    array: [],
+  });
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    fetch("/api", requestOptions)
+      .then((response) => response.json())
+      .then((data) => setBooks({ array: data }));
+    console.log(books);
+  }, []);
+
   return (
     <div>
       <h1>Catalogue</h1>
+      {console.log(books)}
+      {books.array.map(function (book, i) {
+        return <h4>{book.title}</h4>;
+      })}
     </div>
   );
 };

ba8fcfcbaaa388edd037dae7dab84b5a1e357530
jtwob <32282322+jtwob@users.noreply.github.com>
2022-07-24 08:42:36 -0700

    UPDATE: added a react bootstrap compatible boilerplate navbar

--- client/src/Components/Nav/Nav.js
+++ client/src/Components/Nav/Nav.js
@@ -1,17 +1,32 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-function Nav() {
+function NavComponent() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/catalogue">
-        My Books
-      </a>
-      <br />
-      <a className="navbar-brand" href="/home">
-        New Book
-      </a>
-    </nav>
+    <Navbar bg="light" variant="light">
+      <Container>
+        <Navbar.Brand href="#home">Library-App</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/home">New Book</Nav.Link>
+          <Nav.Link href="/catalogue">Catalogue</Nav.Link>
+          <Nav.Link className="disabled" href="">
+            Search
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+    // <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    //   <a className="navbar-brand" href="/catalogue">
+    //     My Books
+    //   </a>
+    //   <br />
+    //   <a className="navbar-brand" href="/home">
+    //     New Book
+    //   </a>
+    // </nav>
   );
 }
 
-export default Nav;
+export default NavComponent;
